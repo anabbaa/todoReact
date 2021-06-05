2b36947ec55671df66ac849ad54fe24aa67c7bb7
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-06-06 00:43:07 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import List from "./components/List";
+import Todo from "./components/Todo";
+function App() {
+  const [list, setList] = useState([]);
+  //we create state here we update in list and manipulate in todo
+  const remove = (itemToRemove) => {
+    // itemToRemove has the id for that specific item
+    const newArr = list.filter((item) => item.id !== itemToRemove);
+    // send all items unless who has the same id with itemtoremov id user which click on
+    setList(newArr);
+  };
+
+  const check = (itemToCheck) => {
+    // itemToCheck has the id for that specific item
+    setList((prevState) => {
+      return prevState.filter((item) => {
+        if (item.id === itemToCheck) item.done = !item.done;
+        return item;
+      });
+    });
+  };
+  // React Fragments allow you to wrap multiple elements without adding an extra node to the DOM. This can be useful when rendering multiple child elements/components in a single parent component.
+  return (
+    <React.Fragment>
+      <Header />
+      <List setList={setList} />
+      <Todo list={list} remove={remove} check={check} />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+const List = (props) => {
+  
+  const [userInput, setUserInput] = useState("");
+  function changeHandle(e) {
+    
+    setUserInput(e.target.value);
+  }
+  function submitHandle(e) {
+    e.preventDefault();
+    // prevState holds on the value of state before any update on the state
+    //console.log(uuidv4());
+    if (userInput.length) {//input.length inorder to dont have array for emty input when its 0 its false
+  props.setList((prevState)=>
+  [...prevState,{id: uuidv4(), title: userInput.trim(), done: false
+
+  },]);
+
+        //another way to add id is to userinput.length but its not perfect delet and add it will add number 
+        //2 twtimes and we will not deletid it transform for another place
+    
+      setUserInput("");
+    }
+  }
+  return (
+    <form onSubmit={submitHandle}>
+      <input className="text" type="text" placeholder="write here your list" value={userInput} onChange={changeHandle} />
+      <input className="add" type="submit" value="Add" />
+    </form>
+  );
+};
+
+export default List;
+
